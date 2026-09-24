1b283feb6152486fbe9ea09abd3db6fb985baa18
Pulkit <your.email@example.com>
2026-09-25 00:47:31 +0530

    Fix PWABuilder PNG icons requirements

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,7 +17,8 @@ export default defineConfig({
         background_color: '#F8FAFC',
         display: 'standalone',
         icons: [
-          { src: '/vite.svg', sizes: 'any', type: 'image/svg+xml', purpose: 'any maskable' }
+          { src: '/icons/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icons/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
         ]
       }
     })
